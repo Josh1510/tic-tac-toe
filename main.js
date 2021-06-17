a4e957d65467a17b0abeed571cfadaf7e76ce16a
Joshua Moon <joshmoon.1510@gmail.com>
2021-06-17 22:49:27 +1000

    adding in user factory function

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 "use strict";
 
+// creation of the gameboard as a module
 const gameBoard = (() => {
   const board = ["x", "o", "x", "", "", "", "", "", ""];
 
+  // returns the current state of the board
   const getBoard = () => board;
 
+  // returns the current symbole located at a specific index
   const getBoardPos = (index) => board[index];
 
+  // sets a specific board position to the users input
   const setBoardPos = (index, player) => (board[index] = player);
 
+  // resets the board for a new game
   const resetBoard = () => {
     for (let i = 0; i < board.length; i++) {
       board[i] = "";
@@ -17,6 +22,13 @@ const gameBoard = (() => {
 
   return { getBoard, getBoardPos, setBoardPos, resetBoard };
 })();
+
+// creates a user object and returns their symbol
+const user = (symbol) => {
+  const getSymbol = () => symbol;
+
+  return { getSymbol };
+};
 
 //console.log(board);
 console.log(gameBoard.getBoard());
